c8376f36cf3444da58ad69c12bc911f356ae4311
Mark Ramsey <mramsey2624@gmail.com>
2023-08-08 20:39:42 -0400

    Initial add

---
+++ index.ts
@@ -0,0 +1,24 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import * as fs from 'fs';
+import * as yaml from 'js-yaml';
+
+async function run() {
+  try {
+    const yamlPath: string = core.getInput('yaml-file', { required: true });
+
+    const yamlContent: any = yaml.load(fs.readFileSync(yamlPath, 'utf8'));
+
+    for (const key in yamlContent) {
+      if (Object.prototype.hasOwnProperty.call(yamlContent, key)) {
+        const element = yamlContent[key];
+        core.setOutput(key, element);
+      }
+    }
+
+  } catch (error:unknown) {
+    core.setFailed("Failed");
+  }
+}
+
+run();
